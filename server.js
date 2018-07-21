82b4b709f4fe2320cb7334282781c52ac38df076
Khalid Saifullah Rony <ksaifrony@gmail.com>
2018-07-21 15:48:32 +0000

    classwork6

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
- var http = require('http');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World\n");
-  });
+var http = require('http');
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var server = http.Server(app);
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
